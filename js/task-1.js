fd32928f475bf930870648101049ae9e2930a79c
Svetakler <svetakler86@gmail.com>
2024-09-11 12:21:36 +0300

    hw4

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,12 @@
+function isEnoughCapacity(products, containerSize) {
+  let totalProducts = 0;
+
+  for (let key in products) {
+    totalProducts += products[key];
+  }
+  return totalProducts <= containerSize;
+}
+
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 
 console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,14 @@
+function calcAverageCalories(days) {
+  if (days.length === 0) {
+    return 0;
+  }
+  let totalCalories = 0;
+  for (let i = 0; i < days.length; i++) {
+    totalCalories += days[i].calories;
+  }
+  return totalCalories / days.length;
+}
+
 console.log(
   calcAverageCalories([
     { day: 'monday', calories: 3010 },
